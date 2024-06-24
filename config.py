2bd69892cabab00f7f9e0e3f38a88e7da0f9b2bc
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-24 18:25:03 +0545

    move file to root directory

--- config.py
+++ config.py
@@ -1,22 +1,31 @@
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent
-GRAPHICS_DIR = ROOT_DIR / 'graphics' / 'graphics'
-ENEMY_SHIT_DIR = GRAPHICS_DIR / 'enemy_ship'
-EXPLOSION_DIR = GRAPHICS_DIR / 'explosion'
-PLAYER_SHIP_DIR = GRAPHICS_DIR / 'player_ship'
-SPACE_BG_DIR = GRAPHICS_DIR / 'space_bg'
+
+GRAPHICS_DIR = ROOT_DIR / "graphics" / "graphics"
+ENEMY_SHIP_DIR = GRAPHICS_DIR / "enemy_ship"
+EXPLOSION_DIR = GRAPHICS_DIR / "explosion"
+PLAYER_SHIP_DIR = GRAPHICS_DIR / "player_ship"
+SPACE_BG_DIR = GRAPHICS_DIR / "space_bg"
+
+path_dict = {
+    "player": PLAYER_SHIP_DIR,
+    "enemy": ENEMY_SHIP_DIR,
+    "spacebg": SPACE_BG_DIR,
+    "explosion": EXPLOSION_DIR,
+}
+
 
 if __name__ == "__main__":
-  all_dirs = [
-    ROOT_DIR,
-    GRAPHICS_DIR,
-    ENEMY_SHIT_DIR,
-    EXPLOSION_DIR,
-    PLAYER_SHIP_DIR,
-    SPACE_BG_DIR
-  ]
+    all_dirs = [
+        ROOT_DIR,
+        GRAPHICS_DIR,
+        ENEMY_SHIP_DIR,
+        EXPLOSION_DIR,
+        PLAYER_SHIP_DIR,
+        SPACE_BG_DIR,
+    ]
 
-  for dir_path in all_dirs:
-    if not dir_path.exists():
-      print(f"{dir_path} not found.")
+    for dir_path in all_dirs:
+        if not dir_path.exists():
+            print(f"{dir_path} not found.")
