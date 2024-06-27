51ef704f6cc620b5e6f995c8a945e9e92e49edbc
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-27 15:45:05 +0545

    correct wrong health amount display

--- src/UI/healthbar.py
+++ src/UI/healthbar.py
@@ -9,13 +9,14 @@ class Healthbar:
         self.filled_health_chunks = load_frame(HEALTH_UI_DIR / "filled")
         self.chunk_width = self.empty_health_chunks[0].get_width()
         self.health_count = health_count
+        self.full_health = health_count
 
     def update_health_count(self, health_count: int):
         self.health_count = health_count
 
     def display(self, display_surface: pygame.Surface):
         i = 0
-        for i in range(self.health_count):
+        for i in range(self.full_health):
             display_surface.blit(
                 self.empty_health_chunks[1], ((i + 1) * self.chunk_width, 0)
             )
