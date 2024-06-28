4d5b31c76908a08e30593049ba84966bea4ca68a
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-28 20:36:41 +0545

    add more resonable name for instance attribute

--- src/storage/storage.py
+++ src/storage/storage.py
@@ -15,26 +15,26 @@ class StorageHandler:
         if self.__instance is not None:
             return
         self.__storage_path: Path = ROOT_DIR / "src" / "storage" / "json"
-        self.__ship_data: dict = {}
-        self.__init_ship_data()
+        self.__game_data: dict = {}
+        self.__init_game_data()
 
         self.__instance = self
 
-    def __init_ship_data(self) -> None:
+    def __init_game_data(self) -> None:
         """
         Initializes ship data from the JSON file. If the file does not exist or is corrupted,
         it writes default data to the file.
         """
-        ship_data_path: Path = self.__storage_path / "ship-data.json"
+        game_data_path: Path = self.__storage_path / "ship-data.json"
         default_data_path: Path = self.__storage_path / "default-data.json"
 
         write_default_data: bool = False
         try:
-            f_ship_data = open(ship_data_path, "r+")
-            ship_data: dict = json.load(f_ship_data)
-            self.__ship_data = ship_data
+            f_game_data = open(game_data_path, "r+")
+            game_data: dict = json.load(f_game_data)
+            self.__game_data = game_data
         except FileNotFoundError:
-            Path(ship_data_path).touch()
+            Path(game_data_path).touch()
             write_default_data = True
         except JSONDecodeError:
             write_default_data = True
@@ -43,9 +43,9 @@ class StorageHandler:
                 return None
             with open(default_data_path, "r") as f_default_data:
                 default_data: dict = json.load(f_default_data)
-                json.dump(obj=default_data, fp=f_ship_data, indent=4)
-                self.__ship_data = default_data
-            f_ship_data.close()
+                json.dump(obj=default_data, fp=f_game_data, indent=4)
+                self.__game_data = default_data
+            f_game_data.close()
 
     def write_player_data(self, data: dict) -> None:
         """
@@ -54,7 +54,7 @@ class StorageHandler:
         Args:
             data (dict): Dictionary containing player data to be updated.
         """
-        self.__ship_data.get("player").update(data)
+        self.__game_data.get("player").update(data)
         self.__write_to_file()
 
     def write_current_level(self, level: int) -> None:
@@ -64,41 +64,41 @@ class StorageHandler:
         Args:
             level (int): The current level to be updated.
         """
-        self.__ship_data["current_level"] = level
+        self.__game_data["current_level"] = level
         self.__write_to_file()
 
     def __write_to_file(self) -> None:
         """
         Writes the current ship data to the JSON file.
         """
-        ship_data_path: Path = self.__storage_path / "ship-data.json"
+        game_data_path: Path = self.__storage_path / "ship-data.json"
 
-        with open(ship_data_path, "w") as f_ship_data:
+        with open(game_data_path, "w") as f_game_data:
             try:
-                json.dump(self.__ship_data, f_ship_data, indent=4)
+                json.dump(self.__game_data, f_game_data, indent=4)
             except (JSONDecodeError, FileNotFoundError):
                 print("Couldn't save data")
 
-    def reset_ship_data(self) -> None:
+    def reset_game_data(self) -> None:
         """
         Resets the ship data to the default values from the default data JSON file.
         """
-        ship_data_path: Path = self.__storage_path / "ship-data.json"
+        game_data_path: Path = self.__storage_path / "ship-data.json"
         default_data_path: Path = self.__storage_path / "default-data.json"
 
         with open(default_data_path, "r") as f_default_data:
             default_data: dict = json.load(f_default_data)
-        with open(ship_data_path, "w") as f_ship_data:
-            json.dump(default_data, f_ship_data, indent=4)
+        with open(game_data_path, "w") as f_game_data:
+            json.dump(default_data, f_game_data, indent=4)
 
-    def get_ship_data(self) -> dict:
+    def get_game_data(self) -> dict:
         """
         Returns the current ship data.
 
         Returns:
             dict: Dictionary containing the ship data.
         """
-        return self.__ship_data
+        return self.__game_data
 
     def get_player_data(self) -> dict:
         """
@@ -107,7 +107,7 @@ class StorageHandler:
         Returns:
             dict: Dictionary containing the player data.
         """
-        return Storage.get_ship_data().get("player")
+        return Storage.get_game_data().get("player")
 
     def get_level_data(self, level: int) -> dict:
         """
@@ -119,7 +119,7 @@ class StorageHandler:
         Returns:
             dict: Dictionary containing the level data.
         """
-        return Storage.get_ship_data().get("levels").get(f"{level}")
+        return Storage.get_game_data().get("levels").get(f"{level}")
 
     def get_current_level(self) -> int:
         """
@@ -128,7 +128,7 @@ class StorageHandler:
         Returns:
             int: The current level.
         """
-        return Storage.get_ship_data().get("current_level")
+        return Storage.get_game_data().get("current_level")
 
     def get_enemy_data(self, type_: Literal["shooter", "self_killer"]) -> dict:
         """
@@ -140,7 +140,7 @@ class StorageHandler:
         Returns:
             dict: Dictionary containing the enemy data.
         """
-        return Storage.get_ship_data().get("enemies").get(type_)
+        return Storage.get_game_data().get("enemies").get(type_)
 
 
 Storage = StorageHandler()
