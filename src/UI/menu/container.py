434c1dce94b5767e94703b5d99a2ef1ab1fd329f
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-29 10:32:55 +0545

    rename graphics directory to assets

--- src/UI/menu/container.py
+++ src/UI/menu/container.py
@@ -9,7 +9,26 @@ class Container:
         path = UI_DIR / "menu" / "common" / "Window.png"
         self.image = load_image(path, None, (WIDTH, HEIGHT))
         self.rect = self.image.get_rect(center=pos)
+        print(self.precalculate_header_boxsize())
+
+    def precalculate_header_boxsize(self):
+        """i just used this way using a threshold to capture nearset family of color
+        to detect position for header in image, this will do work done although its not so good way
+        """
+
+        x, y = self.image.get_width() // 2, 0
+        midtop_color = self.image.get_at((x, y))[0]
+        start_Y, end_y = (None, None)
+
+        while abs(self.image.get_at((x, y))[0] - midtop_color) <= 10:
+            y += 1
+        start_y = y
+        while abs(self.image.get_at((x, y))[0] - midtop_color) > 10:
+            y += 1
+        end_y = y
+
+        return (start, end)
 
     def display(self, display_surface: pygame.Surface):
-        display_surface.fill("grey")
+        display_surface.fill((138, 170, 169))
         display_surface.blit(self.image, self.rect.topleft)
