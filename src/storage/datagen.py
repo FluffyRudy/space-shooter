e714d89e2ffccec9f28933337dfd8bd6da879886
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-07-06 11:30:22 +0545

    update missile initial damage

--- src/storage/datagen.py
+++ src/storage/datagen.py
@@ -16,7 +16,7 @@ def generate_levels_data(num_levels):
         },
         "weapons": {
             "laser": {"speed": 0, "damage": 1, "kill_after": 5000},
-            "missile": {"speed": 5, "damage": 2, "count": 3},
+            "missile": {"speed": 5, "damage": 5, "count": 3},
         },
         "defence": {
             "shield": {"amount": 1, "kill_after": None},
