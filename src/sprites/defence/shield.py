799cf367ec29c558b1549f9b534fd2264747e0f1
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-07-07 18:40:51 +0545

    refactor shield module

--- src/sprites/defence/shield.py
+++ src/sprites/defence/shield.py
@@ -3,8 +3,7 @@ import pygame
 from src.storage.storage import Storage
 from src.utils.image_util import load_frame
 from src.timer.cooldown import Cooldown
-from src.settings import HEIGHT
-from src.constants import SHIELD, AMOUNT, KILL_AFTER
+from src.constants import SHIELD, KILL_AFTER
 from config import DEFENCE_DIR
 
 
@@ -14,18 +13,16 @@ class Shield(pygame.sprite.Sprite):
         groups: list[pygame.sprite.Group],
         pos: tuple[int, int],
     ):
-        POWER_DIR = DEFENCE_DIR / SHIELD
-        ATTRIBUTES = Storage.get_defence(SHIELD)
-        self.amount = ATTRIBUTES.get(AMOUNT)
-
         super().__init__(groups)
-        self.frames = load_frame(POWER_DIR, (0.3, 0.3))
+
+        ATTRIBUTES = Storage.get_defence(SHIELD)
+        self.frames = load_frame(DEFENCE_DIR / SHIELD, (0.3, 0.3))
         self.frame_index = 0
         self.animation_speed = 0.2
 
         self.image = self.frames[0]
-
         self.rect = self.image.get_rect(center=pos)
+
         self.self_kill_cd = Cooldown(ATTRIBUTES.get(KILL_AFTER))
         self.self_kill_cd.reset_time()
 
@@ -35,17 +32,25 @@ class Shield(pygame.sprite.Sprite):
         self.alpha_rate = 255 / ATTRIBUTES.get(KILL_AFTER)
 
     def animate(self):
-        self.frame_index += self.animation_speed
-        frame_index = int(self.frame_index) % len(self.frames)
-        if frame_index == 0:
+        self.frame_index = (self.frame_index + self.animation_speed) % len(self.frames)
+        if int(self.frame_index) == 0:
             self.frame_index = 0
-        self.image = self.frames[frame_index]
+        self.image = self.frames[int(self.frame_index)]
 
     def update(self, *arg, **kwargs):
         self.rect.center = kwargs.get("player").rect.center
 
         self.animate()
         self.self_kill_cd.handle_cooldown()
+        self.handle_alpha()
+
+        if self.self_kill_cd.has_cooldown():
+            kwargs.get("player").make_vulnerable()
+            self.kill()
+        else:
+            kwargs.get("player").make_immune()
+
+    def handle_alpha(self):
         if self.is_brighten:
             self.bright_up_cd.handle_cooldown()
             if self.bright_up_cd.has_cooldown():
@@ -54,14 +59,6 @@ class Shield(pygame.sprite.Sprite):
             elapsed_time = self.self_kill_cd.get_prev_time()
             updated_alpha = max(80, 255 - self.alpha_rate * elapsed_time)
             self.image.set_alpha(int(updated_alpha))
-        if self.self_kill_cd.has_cooldown():
-            kwargs.get("player").make_vulnerable()
-            self.handle_kill()
-        else:
-            kwargs.get("player").make_immune()
-
-    def handle_kill(self):
-        self.kill()
 
     def bright_up(self):
         self.is_brighten = True
